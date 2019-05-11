00e7c030e8b4fd56397640e074e7fa3b86cea7ee
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-11 19:33:11 +0200

    Update app.js file; add tasks routes

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const server = express()
 server.use(express.json())
 
 server.use('/api', require('./routes/welcome'))
+server.use('/api/tasks', require('./routes/task'))
 
 server.use((err, _req, res, _next) => {
   const { status = 500, message } = err
