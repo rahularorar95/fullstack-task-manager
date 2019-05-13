ce12e7ce1944307bacebcbd3f0dfd0f4e56438f4
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-13 17:33:55 +0200

    Update task controller; add a function to create a task

--- server/controllers/task.js
+++ server/controllers/task.js
@@ -10,8 +10,27 @@ const getAllTasks = async (_req, res) => {
   }
 }
 
+const addTask = async ({ value }, res) => {
+  try {
+    const foundTask = await Task.find({ description: value.body.description }).countDocuments()
+
+    if (foundTask) {
+      const error = JSON.stringify({ errors: { description: 'Task already exists' } })
+      throw new Error(error)
+    }
+
+    const task = new Task({ ...value.body })
+    await task.save()
+
+    res.status(201).json(task)
+  } catch (e) {
+    res.status(400).send(e.message)
+  }
+}
+
 const TaskController = {
-  getAllTasks
+  getAllTasks,
+  addTask
 }
 
 module.exports = TaskController
