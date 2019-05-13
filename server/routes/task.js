65fcc378507277d62bc36c17bc8a9650c8c58f20
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-13 17:35:23 +0200

    Update task routes; add route for POST /api/tasks endpoint

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
 
 const TaskController = require('../controllers/task')
+const { add } = require('../validation')
+const validateBody = require('../middlewares/validateBody')
 
 router.get('/', TaskController.getAllTasks)
+
+router.post('/', validateBody(add), TaskController.addTask)
 
 module.exports = router
