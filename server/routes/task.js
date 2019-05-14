b23aecae16ed1ae49f4d1e49f67ea01f2aae6d38
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2019-05-14 15:35:01 +0200

    Update task routes; add route for PATCH /api/tasks/:id endpoint

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,12 +1,14 @@
 const router = require('express').Router()
 
 const TaskController = require('../controllers/task')
-const { add } = require('../validation')
+const { add, edit } = require('../validation')
 const validateBody = require('../middlewares/validateBody')
 
 router.get('/', TaskController.getAllTasks)
 
 router.post('/', validateBody(add), TaskController.addTask)
+
+router.patch('/:id', validateBody(edit), TaskController.updateTask)
 
 router.delete('/:id', TaskController.removeTask)
 
