8fe94d66b44900884ba2cd77430448ad4bf3815f
Titux Metal <github@lgdweb.fr>
2020-04-16 03:55:34 +0200

    Update server config file; add useUnifiedTopology option for mongoose

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   isProd: process.env.NODE_ENV === 'production' || false,
   port: process.env.PORT || 3000,
-  mongoOptions: { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false },
+  mongoOptions: { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true },
   mongoUri: process.env.MONGO_URI || 'mongodb://localhost:27017'
 }
